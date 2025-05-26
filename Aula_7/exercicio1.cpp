def772a8324edfd67e5c7333eddd17eb715ecc34
Slept0122 <luis.henrique.01@hotmail.com>
2025-05-26 11:36:01 -0300

    Aula7

---
+++ Aula_7/exercicio1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> num(10);
+
+int main(){
+
+    cout << "digite 10 valores inteiros para fazer separacao impar ou par" << endl;
+    //cin >> algo;
+}
