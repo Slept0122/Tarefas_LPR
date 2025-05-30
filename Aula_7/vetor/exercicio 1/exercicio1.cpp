21eaef93e277d95a727f8b5615bf30ff335b6d4a
Slept0122 <luis.henrique.01@hotmail.com>
2025-05-30 09:36:26 -0300

    Aula_7

--- Aula_7/vetor/exercicio 1/exercicio1.cpp
+++ Aula_7/vetor/exercicio 1/exercicio1.cpp
@@ -4,9 +4,17 @@
 using namespace std;
 
 vector<int> num(10);
+int escolhido;
 
 int main(){
 
     cout << "digite 10 valores inteiros para fazer separacao impar ou par" << endl;
-    //cin >> algo;
+    cin >> escolhido;
+        for(int i= 0; i > 11 ;i++){
+
+         if(num[i] == 0) escolhido = num[i];
+        }
+        
+ cout << num[i] << endl;
+    
 }
