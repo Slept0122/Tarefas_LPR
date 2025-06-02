5091a780709879966220286c16537940ad670815
Slept0122 <luis.henrique.01@hotmail.com>
2025-06-02 11:37:08 -0300

    Atualizado

--- Aula_7/vetor/exercicio 1/exercicio1.cpp
+++ Aula_7/vetor/exercicio 1/exercicio1.cpp
@@ -4,17 +4,22 @@
 using namespace std;
 
 vector<int> num(10);
-int escolhido;
+vector<int> escolhido(10);
 
-int main(){
-
+int main()
+{
     cout << "digite 10 valores inteiros para fazer separacao impar ou par" << endl;
-    cin >> escolhido;
-        for(int i= 0; i > 11 ;i++){
 
-         if(num[i] == 0) escolhido = num[i];
+    cin >> escolhido[0] >> escolhido[1] >> escolhido[2] >> escolhido[3] >> escolhido[4] >> escolhido[5] >> escolhido[6] >> escolhido[7] >> escolhido[8] >> escolhido[9];
+
+    for (int i = 0; i < 10; i++)
+    {
+
+        if (escolhido[i] % 2 == 0)
+        {
+            cout << "estes numeros sao pares: " << escolhido[i] << endl;
         }
-        
- cout << num[i] << endl;
-    
+        else
+            cout << "estes numeros sao impares: " << escolhido[i] << endl;
+    }
 }
---
+++ Aula_7/vetor/exercicio2/exercicio 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> num(10);
+int escolhido;
+
+int main()
+{
+    cout << "insira um numero em que deseje pesquisar no vetor" << endl;
+    cin >> escolhido;
+}
